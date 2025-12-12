6d0a6b02847980587c82cd4e008f309c0b102fe2
Pradeep Verma <pradeepnetcore@outlook.com>
2025-12-12 14:46:53 +0530

    React Functional Component

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-                    "h1", 
-                    { id: "heading" },
-                     "Hello World from React"
-                    );
-  //JXS - HTML like syntax     
-  // JXS (transpiled before it reaches to the React/JS Engine)  --> React.createElement()          
-  // Babel is used to transpile the JSX code to React.createElement() code
-  // Parcel Bundler comes with Babel pre-configured
-  // Parcel is used to bundle the code and its dependencies into a single file for the browser to understand
-  // Parcel also comes with a development server that hot reloads the code on changes
-const jsxHeading = <h1 id='heading'>Hello React using JSX</h1>                   
+// React Element using JSX
+const heading = (
+  <h1 id="heading" className="head">
+    Hello React using JSX !
+  </h1>
+);
 
+//React Component
+// Class based component - old
+// Functional component - new
+
+// React Functional Component - just a JS function that returns React Element
+// A function returning JSX is called Functional Component
+// Component name should start with a capital letter
+const HeadingComponent = () => {
+  return <h1>Hello React Functional Component</h1>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
-
